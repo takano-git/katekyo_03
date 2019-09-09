61020d8b3356d1d27f08ba2bb68b88a02bb7926d
takano <pami3025@gmail.com>
2019-09-09 03:51:53 +0000

    6.5ユーザー登録と同時にログインする

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,4 +10,9 @@ module SessionsHelper
       @current_user ||= User.find_by(id: session[:user_id])
     end
   end
+  
+  #現在ログイン中のユーザーがいればtrue,そうでなければfalseを返します。
+  def logged_in?
+    !current_user.nil?
+  end
 end
