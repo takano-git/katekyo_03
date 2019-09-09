1964cab1924bc4cfde6d9ef1117c6d60653c4cdc
takano <pami3025@gmail.com>
2019-09-09 05:53:52 +0000

    7.1.4 ユーザーの記憶を忘れる

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,4 +38,9 @@ class User < ApplicationRecord
   def authenticated?(remember_token)
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
+  
+  # ユーザーのログイン情報の破棄します
+  def forget
+    update_attribute(:remember_digest,nil)
+  end
 end
