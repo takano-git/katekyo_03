8bff21e2080818b6e4c733b4ce0d19eb26d1c54a
takano <pami3025@gmail.com>
2019-09-09 05:22:02 +0000

    7.1.2rememberメソッドを作ろう

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  
+  # 「remember_token」という仮想の属性を作成します。
+  attr_accessor :remember_token
   before_save { self.email = email.downcase}
   
   validates :name, presence: true, length: { maximum:50 }
@@ -23,5 +26,11 @@ class User < ApplicationRecord
   # ランダムなトークンを返します。
   def User.new_token
     SecureRandom.urlsafe_base64
+  end
+  
+  # 永続セッションのためのハッシュ化したトークンをデータベースに記憶します。
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 end
