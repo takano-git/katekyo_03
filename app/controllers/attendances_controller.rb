c15ebec2cd811a00668f02b9397212f9d429928d
takano <pami3025@gmail.com>
2019-09-14 03:07:18 +0000

    11.1.3追加

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,4 +1,9 @@
 class AttendancesController < ApplicationController
+  before_action :set_user, only: :edit_one_month
+  before_action :logged_in_user, only: [:update,:edit_one_month]
+  before_action :set_one_month,only: :edit_one_month
+  
+  
   UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直してください。"
   
   def update
@@ -9,7 +14,7 @@ class AttendancesController < ApplicationController
       if @attendance.update_attributes(started_at: Time.current.change(sec:0))
         flash[:info] = "おはようございます！"
       else
-        flas[:danger] = UPDATE_ERROR_MSG
+        flash[:danger] = UPDATE_ERROR_MSG
       end
     elsif @attendance.finished_at.nil?
       if @attendance.update_attributes(finished_at: Time.current.change(sec: 0))
@@ -20,4 +25,8 @@ class AttendancesController < ApplicationController
     end
     redirect_to @user
   end
+  
+  def edit_one_month
+  end
+  
 end
